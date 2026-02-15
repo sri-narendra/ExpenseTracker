fe85bbafd3057eb6213885f419660ae5003610b2
sri-narendra <srinarendraofficial@gmail.com>
2026-02-16 02:32:17 +0530

    Delete projects/expencetracker/expencetracker directory

--- projects/expencetracker/expencetracker/server/config/categories.js
+++
@@ -1,9 +0,0 @@
-const categories = [
-    "Food", "Travel", "Transport", "Bills", "Shopping", "Housing", 
-    "Utilities", "Entertainment", "Health", "Other",
-    "Salary", "Business", "Freelance", "Investments", "Gift"
-];
-
-module.exports = {
-    categories
-};
