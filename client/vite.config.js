7cd4f0d377ff61b1a6c167a3fefbde7d9cead2f1
sri-narendra <srinarendraofficial@gmail.com>
2026-02-16 03:50:50 +0530

    Deployment: Final fix for 404 and absolute path leaks using absolute base

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/ExpenseTracker/',
   build: {
     outDir: '../',
     emptyOutDir: false, // Don't delete everything in the root!
